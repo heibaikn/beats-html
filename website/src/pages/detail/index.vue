03634f9d2ae27230a5c026ea8e6a2820148b8e00
qiqinghao <qqhluckyi@163.com>
2019-03-17 22:52:37 +0800

    详情商品放大镜

--- website/src/pages/detail/index.vue
+++ website/src/pages/detail/index.vue
@@ -7,17 +7,14 @@
         <div class="product-content">
           <div class="product-content__images">
             <div class="main-image">
-              <img src="../../assets/detail-img.png" alt="">
+              <pic-zoom :url="detailImg" :scale="3"></pic-zoom>
             </div>
             <div class="small-list">
-              <div class="item"><img src="../../assets/detail-img.png" alt=""></div>
-              <div class="item"><img src="../../assets/detail-img.png" alt=""></div>
-              <div class="item"><img src="../../assets/detail-img.png" alt=""></div>
+              <div class="item" :class="{active: currIndex == index}" @click="toggleImage(img, index)" v-for="(img, index) in imgsList" :key="index"><img :src="img" alt=""></div>
             </div>
           </div>
           <div class="product-content__info">
             <h1>BeatsX 入耳式耳机 - 丝缎银</h1>
-            <!-- <p class="price">RMB 899</p> -->
             <div class="price-box">
               <p>价格：<del class="font">¥88</del></p>
               <p>促销价：<span class="red font font-fs">¥<span class="tm-price">119.00</span></span></p>
@@ -38,32 +35,44 @@
 </template>
 
 <script>
+import PicZoom from '@shared/components/PicZoom'
 import mainHeader from '@shared/components/header'
 import mainFooter from '@shared/components/footer'
 import { getUserInfo } from '@/api'
-import pageBanner from '../../assets/page-img.jpg'
+import detailImg from '../../assets/detail-img.png'
+import detailImg3 from '../../assets/img3.png'
 
 export default {
   name: 'headPhones',
   data(){
     return{
-      
+      currIndex: 0,
+      detailImg: '',
+      imgsList: [
+        detailImg,
+        detailImg3
+      ]
     }
   },
   components: {
     mainHeader,
-    mainFooter
+    mainFooter,
+    PicZoom
   },
   computed: {
     
   },
   created(){
-    getUserInfo().then(d=>{
-      console.log(d)
-    })
+    this.detailImg = this.imgsList[0]
+    // getUserInfo().then(d=>{
+    //   console.log(d)
+    // })
   },
   methods: {
-    
+    toggleImage(img, index){
+      this.detailImg = img
+      this.currIndex = index
+    }
   },
 }
 </script>
